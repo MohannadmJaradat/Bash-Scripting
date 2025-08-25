87a7139b0db220ee05e088e61a11c9d2f847ea91
Mohannad Jaradat <jaradatm2@hotmail.com>
2025-08-25 22:42:27 +0300

    variables

---
+++ bash-scripts/variable2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+word="awesome"
+
+echo "Linux is ${word}"
+echo "Video games are ${word}"
+echo "Sunny days are ${word}"
+
+# This is how variables can actually be helpful in production.
