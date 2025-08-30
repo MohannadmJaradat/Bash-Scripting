87956151de4b6008d50b73d6f9421e3fda4f7b41
mohannadmjaradat <jaradatm2@hotmail.com>
2025-08-30 18:20:57 +0300

    universal update v1.2

--- bash-scripts/universal-update.sh
+++ bash-scripts/universal-update.sh
@@ -8,9 +8,17 @@ then
     sudo pacman -Syu
 fi
 
+if grep -q "Debian" ${release_file}
+then
+    # The host is based on Debian,
+    # Run the apt version of the command
+    sudo apt update
+    sudo apt dist-upgrade
+fi
+
 if grep -q "Ubuntu" ${release_file}
 then
-    # The host is based on Debian or Ubuntu
+    # The host is based on Ubuntu,
     # Run the apt version of the command
     sudo apt update
     sudo apt dist-upgrade
