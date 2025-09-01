96b9148cc18e5613ed6ee3ba7a8cb674589894c8
mohannadmjaradat <jaradatm2@hotmail.com>
2025-09-02 00:00:30 +0300

    data streams update

--- bash-scripts/universal-update.sh
+++ bash-scripts/universal-update.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if grep -q "Arch" ${release_file}
 then
     # The host is based on Arch, run the pacman update command
-    sudo pacman -Syu
+    sudo pacman -Syu 1>>${logfile} 2>>${errorlog}
+    if [ $? -ne 0 ]
+    then 
+        echo "An error occurred, please check the ${errorlog} file."
+    fi
 fi
 
 if grep -q "Debian" ${release_file} || grep -q "Ubuntu" ${release_file}
 then
     # The host is based on Ubnutu,
     # Run the apt version of the command
-    sudo apt update
-    sudo apt dist-upgrade
+    sudo apt update 1>>${logfile} 2>>${errorlog}
+    if [ $? -ne 0 ]
+    then 
+        echo "An error occurred, please check the ${errorlog} file."
+    fi
+    sudo apt dist-upgrade -y 1>>${logfile} 2>>${errorlog}
+    if [ $? -ne 0 ]
+    then 
+        echo "An error occurred, please check the ${errorlog} file."
+    fi
 fi
