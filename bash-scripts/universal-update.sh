b05689c626549cb275451eaad124b59ffb98c0d7
mohannadmjaradat <jaradatm2@hotmail.com>
2025-09-03 22:45:36 +0300

    using functions with our universal-update script

--- bash-scripts/universal-update.sh
+++ bash-scripts/universal-update.sh
@@ -4,14 +4,18 @@ release_file=/etc/os-release
 logfile=/var/log/updater.log
 errorlog=/var/log/updater_errors.log
 
-if grep -q "Arch" ${release_file}
-then
-    # The host is based on Arch, run the pacman update command
-    sudo pacman -Syu 1>>${logfile} 2>>${errorlog}
+check_exit_status() {
     if [ $? -ne 0 ]
     then 
         echo "An error occurred, please check the ${errorlog} file."
     fi
+}
+
+if grep -q "Arch" ${release_file}
+then
+    # The host is based on Arch, run the pacman update command
+    sudo pacman -Syu 1>>${logfile} 2>>${errorlog}
+    check_exit_status
 fi
 
 if grep -q "Debian" ${release_file} || grep -q "Ubuntu" ${release_file}
@@ -19,13 +23,7 @@ then
     # The host is based on Ubnutu,
     # Run the apt version of the command
     sudo apt update 1>>${logfile} 2>>${errorlog}
-    if [ $? -ne 0 ]
-    then 
-        echo "An error occurred, please check the ${errorlog} file."
-    fi
+    check_exit_status
     sudo apt dist-upgrade -y 1>>${logfile} 2>>${errorlog}
-    if [ $? -ne 0 ]
-    then 
-        echo "An error occurred, please check the ${errorlog} file."
-    fi
+    check_exit_status
 fi
